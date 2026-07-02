8e9788086f7103a616df1329b374986b594c6041
Raúl Albín <raul.albin@vonage.com>
2026-07-02 11:36:56 +0200

    Upgrade to versioin 2.34.1

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenTokTransformers",
-            url: "https://s3.us-east-1.amazonaws.com/artifact.tokbox.com/rel/otkit-ios-sdk-xcframework/vonage-ios-sdk-transformers-2.33.0.zip",
-            checksum: "ccd83ef0a9d2f9cd3754e465e3c50e0b9f50e74fa5c949378cf2b9ede4a6dfbe"
+            url: "https://s3.us-east-1.amazonaws.com/artifact.tokbox.com/rel/otkit-ios-sdk-xcframework/vonage-ios-sdk-transformers-2.34.1.zip",
+            checksum: "58ab756724820f7c5422d0c75c0ced3f072a579e64653968d9a1c7468877baa3 "
         ),
         .target(
             name: "VonageClientSDKVideoTransformers",
